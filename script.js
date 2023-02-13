c082a638cd8216b8ab065fd5ede7a97d24f71f4c
Drew Anello <drew.anello@gmail.com>
2023-02-13 12:43:26 -0500

    game is working console.logs(are out and number was brought down to 25

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 const btn = document.getElementById('btn')
 const inp = document.getElementById('inp')
-const number = Math.floor(Math.random() * 101) + 1
+const number = Math.floor(Math.random() * 25) + 1
 const ul = document.querySelector('ul')
-let resetBtn = null
 
 btn.addEventListener('click', function (e) {
   e.preventDefault()
   const li = document.createElement('li')
-  console.log(e.target)
-  console.log(number)
   if (+inp.value === number) {
     var banner = document.getElementById('banner')
     banner.classList.remove('hidden')
 
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 20; i++) {
       var firework = document.createElement('div')
       firework.classList.add('firework')
-      firework.style.left = Math.random() * 100 + '%'
+      firework.style.left = Math.random() * 25 + '%'
       firework.style.animationDelay = Math.random() * 10 + 's'
       document.getElementById('fireworks').appendChild(firework)
     }
