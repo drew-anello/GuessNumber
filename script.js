9ab56a9ad0f9ad0d4e3711135cac8fcb1e3d624e
Drew Anello <drew.anello@gmail.com>
2023-02-13 12:27:49 -0500

    adds fireworks and banner when user gueses right

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const btn = document.getElementById('btn')
 const inp = document.getElementById('inp')
 const number = Math.floor(Math.random() * 101) + 1
+const ul = document.querySelector('ul')
 
 btn.addEventListener('click', function (e) {
   e.preventDefault()
+  const li = document.createElement('li')
   console.log(e.target)
   console.log(number)
   if (+inp.value === number) {
-    console.log('you guessed the right number')
+    var banner = document.getElementById('banner')
+    banner.classList.remove('hidden')
+
+    for (var i = 0; i < 100; i++) {
+      var firework = document.createElement('div')
+      firework.classList.add('firework')
+      firework.style.left = Math.random() * 100 + '%'
+      firework.style.animationDelay = Math.random() * 10 + 's'
+      document.getElementById('fireworks').appendChild(firework)
+    }
   } else {
     console.log('try again')
   }
