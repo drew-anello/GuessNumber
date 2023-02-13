a0fd214e160d1d4d57f22cf7a144c25c46ec7e48
Drew Anello <drew.anello@gmail.com>
2023-02-13 11:29:28 -0500

    text input is converting to a number

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const btn = document.getElementById('btn')
+const inp = document.getElementById('inp')
+const number = Math.floor(Math.random() * 101) + 1
 
 btn.addEventListener('click', function (e) {
-  e.preventDefault
+  e.preventDefault()
   console.log(e.target)
+  console.log(number)
+  if (+inp.value === number) {
+    console.log('you guessed the right number')
+  } else {
+    console.log('try again')
+  }
 })
